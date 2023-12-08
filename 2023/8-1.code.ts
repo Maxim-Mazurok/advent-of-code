8a6e10af5ee433d59b872dfe38cc230625127378
Maxim Mazurok <maxim@mazurok.com>
2023-12-08 16:21:13 +1100

    day 8 part 1 - done

---
+++ 2023/8-1.code.ts
@@ -0,0 +1,58 @@
+type Direction = "R" | "L";
+type NodeName = string;
+type Node = {
+  name: NodeName;
+  right: NodeName;
+  left: NodeName;
+};
+
+export const parseInput = (
+  input: string
+): {
+  directions: Direction[];
+  nodes: Node[];
+} => {
+  const lines = input
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line.length > 0);
+
+  const directions = lines[0]!.split("") as Direction[];
+  const nodes = lines.slice(1).map((line) => {
+    const [name, leftRight] = line.split(" = ");
+    const [left, right] = leftRight!
+      .slice(1, -1)
+      .split(", ")
+      .map((name) => name.trim()) as [NodeName, NodeName];
+    return { name: name!, right, left };
+  });
+
+  return { directions, nodes };
+};
+
+export const main = (input: string) => {
+  const { directions, nodes } = parseInput(input);
+
+  let directionsNumber = directions.length;
+  let currentNodeName = "AAA";
+  let steps = 0;
+  do {
+    const currentDirection = directions[steps % directionsNumber];
+    // console.log(currentNodeName, steps, currentDirection);
+
+    if (currentDirection === "R") {
+      currentNodeName = nodes.find(
+        (node) => node.name === currentNodeName
+      )!.right;
+    } else {
+      currentNodeName = nodes.find(
+        (node) => node.name === currentNodeName
+      )!.left;
+    }
+    steps++;
+  } while (currentNodeName !== "ZZZ");
+
+  console.log(currentNodeName);
+
+  return steps;
+};
---
+++ 2023/8-1.spec.ts
@@ -0,0 +1,42 @@
+import dedent from "dedent";
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { expect, it } from "vitest";
+import { main, parseInput } from "./8-1.code";
+
+const sampleInput = dedent(`
+  RL
+
+  AAA = (BBB, CCC)
+  BBB = (DDD, EEE)
+  CCC = (ZZZ, GGG)
+  DDD = (DDD, DDD)
+  EEE = (EEE, EEE)
+  GGG = (GGG, GGG)
+  ZZZ = (ZZZ, ZZZ)
+`);
+
+it("parses input", () => {
+  const result = parseInput(sampleInput);
+  expect(result).toEqual({
+    directions: ["R", "L"],
+    nodes: [
+      { name: "AAA", left: "BBB", right: "CCC" },
+      { name: "BBB", left: "DDD", right: "EEE" },
+      { name: "CCC", left: "ZZZ", right: "GGG" },
+      { name: "DDD", left: "DDD", right: "DDD" },
+      { name: "EEE", left: "EEE", right: "EEE" },
+      { name: "GGG", left: "GGG", right: "GGG" },
+      { name: "ZZZ", left: "ZZZ", right: "ZZZ" },
+    ],
+  });
+});
+
+it("works for example input", () => {
+  expect(main(sampleInput)).toBe(2);
+});
+
+it("works for real input", async () => {
+  const input = await readFile(join(__dirname, "8.input.txt"), "utf-8");
+  expect(main(input)).toBe(18113);
+});
